1630027a520649cde4f14cffd639c8086e38d062
Thomas Gassmann <thomas.gassmann@hotmail.com>
2018-04-29 22:51:01 +0200

    added noprogress tracker

--- src/mycloud/backup.py
+++ src/mycloud/backup.py
@@ -2,14 +2,14 @@ import argparse, os, sys
 from upload import Uploader
 from download import Downloader
 from mycloudapi import get_bearer_token, ObjectResourceBuilder
-from progress import ProgressTracker, LazyCloudProgressTracker, FileProgressTracker, CloudProgressTracker
+from progress import ProgressTracker, LazyCloudProgressTracker, FileProgressTracker, CloudProgressTracker, NoProgressTracker
 
 
 parser = argparse.ArgumentParser(description='Swisscom myCloud Backup')
 parser.add_argument('--direction', metavar='i', type=str, help='The direction of the backup (1 = up, 0 = down')
 parser.add_argument('--local_dir', metavar='d', type=str, help='The directory to backup')
 parser.add_argument('--mycloud_dir', metavar='m', type=str, help='Base path in my cloud to upload to (must start with /Drive/)')
-parser.add_argument('--progress_type', metavar='v', type=str, help='Progress type (CLOUD: Use files in cloud, LAZY_CLOUD: Use files in cloud lazily, FILE: Use local file, progress_file parameter required')
+parser.add_argument('--progress_type', metavar='v', type=str, help='Progress type (CLOUD: Use files in cloud, LAZY_CLOUD: Use files in cloud lazily, FILE: Use local file, progress_file parameter required, NONE: no progress')
 parser.add_argument('--progress_file', metavar='p', type=str, help='Save progress file name')
 parser.add_argument('--encryption_pwd', metavar='e', type=str, help='Encryption Password')
 parser.add_argument('--token', metavar='t', type=str, help='Pass token manually')
@@ -24,7 +24,9 @@ if args.progress_type is None or not (args.direction == '1' or args.direction ==
 bearer = get_bearer_token() if args.token is None else args.token
 is_encrypted = args.encryption_pwd is not None
 
-if args.progress_type == 'CLOUD' and args.direction == '1':
+if args.progress_type == 'NONE':
+    tracker = NoProgressTracker()
+elif args.progress_type == 'CLOUD' and args.direction == '1':
     tracker = CloudProgressTracker(bearer, args.mycloud_dir)
 elif args.progress_type == 'LAZY_CLOUD' and args.direction == '1':
     tracker = LazyCloudProgressTracker(bearer)
--- src/mycloud/progress/__init__.py
+++ src/mycloud/progress/__init__.py
@@ -2,5 +2,6 @@ from progress.progress_tracker import ProgressTracker
 from progress.cloud_progress_tracker import CloudProgressTracker
 from progress.file_progress_tracker import FileProgressTracker
 from progress.lazy_cloud_progress_tracker import LazyCloudProgressTracker
+from progress.no_progress import NoProgressTracker
 
-__all__ = ['ProgressTracker', 'CloudProgressTracker', 'FileProgressTracker', 'LazyCloudProgressTracker']
+__all__ = [ProgressTracker, CloudProgressTracker, FileProgressTracker, LazyCloudProgressTracker, NoProgressTracker]
