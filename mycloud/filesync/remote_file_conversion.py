f01bc2772c02d9b7c6dda5025f33c32388fbc976
Thomas Gassmann <thomas.gassmann@hotmail.com>
2018-08-26 14:50:47 +0200

    updated multi-threading with remote file conversion to improve efficiency

--- mycloud/filesync/remote_file_conversion.py
+++ mycloud/filesync/remote_file_conversion.py
@@ -1,8 +1,9 @@
 import os
 import sys
-from collections import deque
+from typing import List
 from random import shuffle
 from threading import Thread
+from mycloud.helper import operation_timeout
 from mycloud.mycloudapi import MyCloudRequestExecutor, ObjectResourceBuilder, RenameRequest, MetadataRequest
 from mycloud.filesystem import (
     FileManager,
@@ -38,40 +39,65 @@ def convert_remote_files(request_executor: MyCloudRequestExecutor,
                          skip):
     resource_builder = ObjectResourceBuilder(local_dir, mycloud_dir)
     generator = list_candidates_recursively(request_executor, mycloud_dir)
+
     def _skip(file):
         for item in skip:
             if file.startswith(item):
                 return True
         return False
-    threads = deque()
+    threads = []
+    thread_file_sizes = {}
     for is_partial, files in generator:
         try:
             def convert(is_partial, files, request_executor, local_dir, mycloud_dir, _skip):
                 if is_partial:
-                    convert_partials(request_executor, local_dir, mycloud_dir, files, _skip)
+                    convert_partials(request_executor, local_dir,
+                                     mycloud_dir, files, _skip)
                 else:
-                    convert_file(request_executor, local_dir, mycloud_dir, files[0], _skip)
+                    convert_file(request_executor, local_dir,
+                                 mycloud_dir, files[0], _skip)
 
-            if sys.platform == 'win32':
-                thread = Thread(target=convert, args=(is_partial, files, request_executor, local_dir, mycloud_dir, _skip))
-                thread.start()
-                threads.append(thread)
-            else:
-                # TODO: temporarily disabled threading on linux because SIGALRM interrupt does not support to be called in a thread
-                convert(is_partial, files, request_executor, local_dir, mycloud_dir, _skip)
+            local_file = get_local_file(
+                is_partial, files, mycloud_dir, local_dir)
+            file_size = operation_timeout(
+                lambda x: os.stat(x['path']).st_size if os.path.isfile(x['path']) else 0, path=local_file)
+            thread = Thread(target=convert, args=(
+                is_partial, files, request_executor, local_dir, mycloud_dir, _skip))
+            thread.start()
+            thread_file_sizes[thread.ident] = file_size
+            threads.append(thread)
         except TimeoutException:
             log('Timeout while accessing resources', error=True)
         except Exception as ex:
             log(str(ex), error=True)
 
-        # TODO: Handle case where we pop off the longest running thread.
-        # In that case all other threads will finish before it and only one file will be hashed at once
-        if len(threads) >= MAX_THREADS_FOR_REMOTE_FILE_CONVERSION:
-            thread_to_join = threads.popleft()
-            if thread_to_join is None:
-                continue
+        for thread in threads:
+            if not thread.is_alive():
+                threads.remove(thread)
+                del thread_file_sizes[thread.ident]
 
-            thread_to_join.join()
+        if len(threads) >= MAX_THREADS_FOR_REMOTE_FILE_CONVERSION:
+            min_file_size_thread = min(
+                thread_file_sizes, key=thread_file_sizes.get)
+            thread = list(filter(lambda t: t.ident ==
+                                 min_file_size_thread, threads))[0]
+            thread.join()
+            threads.remove(thread)
+            del thread_file_sizes[thread.ident]
+
+
+def get_local_file(is_partial: bool, files: List[str], remote_dir: str, local_dir: str):
+    resource_builder = ObjectResourceBuilder(local_dir, remote_dir)
+    if not is_partial:
+        return resource_builder.build_local_file(files[0])
+    else:
+        base_directories = [os.path.dirname(file) for file in files]
+        distinct_base_directories = set(base_directories)
+        if len(distinct_base_directories) != 1:
+            raise ValueError('A file cannot have multiple base directories')
+        base_directory = base_directories[0]
+        return resource_builder.build_local_file(base_directory, remove_extension=False)
+
 
 def convert_partials(request_executor: MyCloudRequestExecutor,
                      local_dir: str,
@@ -81,15 +107,17 @@ def convert_partials(request_executor: MyCloudRequestExecutor,
     base_directory = os.path.dirname(files[0])
     log('Converting partial files in {}...'.format(base_directory))
     resource_builder = ObjectResourceBuilder(local_dir, remote_dir)
-    local_file = resource_builder.build_local_file(base_directory, remove_extension=False)
-    log('Mapped partial files in directory {} to local file {}'.format(base_directory, local_file))
+    local_file = get_local_file(True, files, remote_dir, local_dir)
+    log('Mapped partial files in directory {} to local file {}'.format(
+        base_directory, local_file))
 
     translatable_path, version = _get_path_and_version_for_local_file(local_file,
                                                                       base_directory,
                                                                       resource_builder,
                                                                       skip_fn(local_file))
 
-    versioned_stream_accessor = VersionedCloudStreamAccessor(translatable_path, version, None)
+    versioned_stream_accessor = VersionedCloudStreamAccessor(
+        translatable_path, version, None)
     sorted_files = sorted(files)
     parts = []
     for sorted_file in sorted_files:
@@ -99,7 +127,8 @@ def convert_partials(request_executor: MyCloudRequestExecutor,
             partial_file = versioned_stream_accessor.get_part_file(index)
             parts.append(partial_file)
 
-            rename_request = RenameRequest(sorted_file, partial_file, is_file=True)
+            rename_request = RenameRequest(
+                sorted_file, partial_file, is_file=True)
             _ = request_executor.execute_request(rename_request)
 
         else:
@@ -121,7 +150,7 @@ def convert_file(request_executor: MyCloudRequestExecutor,
                  skip_fn):
     log('Converting file {}...'.format(remote_file))
     resource_builder = ObjectResourceBuilder(local_dir, remote_dir)
-    local_file = resource_builder.build_local_file(remote_file)
+    local_file = get_local_file(False, [remote_file], remote_dir, local_dir)
     log('Mapped {} to local file {}'.format(remote_file, local_file))
 
     remote_file_without_aes_extension = remote_file
@@ -129,24 +158,27 @@ def convert_file(request_executor: MyCloudRequestExecutor,
         remote_file_without_aes_extension = remote_file[:-len(AES_EXTENSION)]
 
     translatable_path, version = _get_path_and_version_for_local_file(local_file,
-                                                                    remote_file_without_aes_extension,
-                                                                    resource_builder,
-                                                                    skip_fn(local_file))
+                                                                      remote_file_without_aes_extension,
+                                                                      resource_builder,
+                                                                      skip_fn(local_file))
 
     TEMP_FILE_EXTENSION = '.temporary'
-    versioned_stream_accessor = VersionedCloudStreamAccessor(translatable_path, version, None)
+    versioned_stream_accessor = VersionedCloudStreamAccessor(
+        translatable_path, version, None)
     partial_destination = versioned_stream_accessor.get_part_file(0)
 
     temporary_file = remote_file + TEMP_FILE_EXTENSION
     log('Renaming filxe {} to {}...'.format(remote_file, partial_destination))
     while True:
-        rename_request = RenameRequest(remote_file, temporary_file, is_file=True, ignore_conflict=True)
+        rename_request = RenameRequest(
+            remote_file, temporary_file, is_file=True, ignore_conflict=True)
         response = request_executor.execute_request(rename_request)
         if response.status_code == 409:
             temporary_file += TEMP_FILE_EXTENSION
         else:
             break
-    rename_request = RenameRequest(temporary_file, partial_destination, is_file=True, ignore_conflict=True)
+    rename_request = RenameRequest(
+        temporary_file, partial_destination, is_file=True, ignore_conflict=True)
     response = request_executor.execute_request(rename_request)
     if response.status_code == 409:
         log('', error=True)
@@ -179,7 +211,8 @@ def list_candidates_recursively(request_executor: MyCloudRequestExecutor, myclou
     (dirs, files) = MetadataRequest.format_response(response)
     if len(files) == 1 and files[0]['Name'] == METADATA_FILE_NAME and len(dirs) > 0:
         metadata_path = files[0]['Path']
-        log('Skipping {} and subdirectories, because it was already converted...'.format(metadata_path))
+        log('Skipping {} and subdirectories, because it was already converted...'.format(
+            metadata_path))
         return
 
     partial_directory = _is_partial_directory(files)
@@ -209,13 +242,15 @@ def _is_partial_directory(files):
 
 def _get_path_and_version_for_local_file(local_file: str, remote_file: str, resource_builder: ObjectResourceBuilder, no_hash: bool = False):
     if not os.path.isfile(local_file) or no_hash:
-        log('File {} not found. Defaulting to version {}'.format(local_file, DEFAULT_VERSION))
+        log('File {} not found. Defaulting to version {}'.format(
+            local_file, DEFAULT_VERSION))
         version = BasicStringVersion(DEFAULT_VERSION)
         translatable_path = BasicRemotePath(remote_file)
     else:
         log('Found local file {}. Using hash calculated version and uploading properties...'.format(local_file))
         version = HashCalculatedVersion(local_file)
-        translatable_path = LocalTranslatablePath(resource_builder, local_file, version)
+        translatable_path = LocalTranslatablePath(
+            resource_builder, local_file, version)
     return translatable_path, version
 
 
