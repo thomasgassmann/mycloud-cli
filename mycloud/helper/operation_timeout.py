043dcdf7e74e608831a58a1c0c267363125ef970
Thomas Gassmann <thomas.gassmann@hotmail.com>
2018-08-26 21:48:35 +0200

    simplified operation timeout

--- mycloud/helper/operation_timeout.py
+++ mycloud/helper/operation_timeout.py
@@ -1,6 +1,6 @@
 from sys import platform
-import signal
 import threading
+from mycloud.constants import MAX_TIMEOUT
 
 
 class TimeoutException(Exception):
@@ -9,14 +9,10 @@ class TimeoutException(Exception):
 
 
 def operation_timeout(operation, **kwargs):
-    timeout = 15
-    if platform == 'win32':
-        return _operation_timeout_win32(operation, timeout, values=kwargs)
-    else:
-        return _operation_timeout_sigalrm(operation, timeout, values=kwargs)
+    return _operation_timeout(operation, MAX_TIMEOUT, values=kwargs)
 
 
-def _operation_timeout_win32(operation, timeout, values):
+def _operation_timeout(operation, timeout, values):
     class FuncThread(threading.Thread):
 
         def __init__(self):
@@ -33,21 +29,3 @@ def _operation_timeout_win32(operation, timeout, values):
         raise TimeoutException
     else:
         return func_thread.result
-
-
-def _operation_timeout_sigalrm(operation, timeout, values):
-    def handler(first, second):
-        raise TimeoutException
-    # pylint: disable=locally-disabled, no-member
-    signal.signal(signal.SIGALRM, handler)
-    signal.alarm(timeout)
-    ret = None
-    try:
-        ret = operation(values)
-    except TimeoutException:
-        pass
-
-    signal.alarm(0)
-    if ret is None:
-        raise TimeoutException
-    return ret
