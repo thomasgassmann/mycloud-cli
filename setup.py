a200f0116ed75d00d238d0f8134065f23c31bec1
Thomas Gassmann <thomas.gassmann@hotmail.com>
2018-11-11 01:03:02 +0100

    update setup.py

--- setup.py
+++ setup.py
@@ -1,20 +1,24 @@
 from setuptools import setup
 
 
+with open('requirements.txt', 'r') as f:
+    install_reqs = [
+        s for s in [
+            line.strip(' \n') for line in f
+        ] if not s.startswith('#') and s != ''
+    ]
+
+
 setup(
-    name='myCloudBackup',
+    name='mycloud-cli',
     version='1.0.0',
     license='MIT',
     author='Thomas Gassmann',
     author_email='thomas.gassmann@hotmail.com',
     url='https://github.com/ThomasGassmann/swisscom-my-cloud-backup',
-    py_modules=[
-        # TODO
-    ],
-    install_requires=[
-        # TODO
-    ],
+    py_modules=['mycloud'],
+    install_requires=install_reqs,
     entry_points={
-        'console_scripts': 'TODO'
+        'console_scripts': {'console_scripts': ['mycloud=mycloud:main']}
     }
 )
