3b9cfdd0ba9f503672585d5887ec43f9547b5bd2
Thomas Gassmann <thomas.gassmann@hotmail.com>
2019-01-27 23:11:31 +0100

    split selenium proxy from token fetching implementation

---
+++ mycloud/mycloudapi/auth/selenium_proxy.py
@@ -0,0 +1,87 @@
+import os
+from multiprocessing import Process
+from bs4 import BeautifulSoup
+from mitmproxy import proxy, options, http
+from mitmproxy.tools.dump import DumpMaster
+from selenium import webdriver
+
+
+JS_FILE = 'undetectable-headless-browser.js'
+PROXY_HOST = '127.0.0.1'
+PROXY_PORT = 9090
+CHROME = 'chromium'
+CHROME_DRIVER = 'chromedriver'
+
+
+class ProxySelenium:
+
+    def __init__(self, headless: bool):
+        self._get_web_driver(headless)
+        self._run_proxy()
+
+
+    def __enter__(self):
+        return self._driver
+
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self._driver.quit()
+        self._proxy_process.terminate()
+
+
+    def _run_proxy(self):
+        def _wrapper():
+            opts = options.Options(listen_host=PROXY_HOST, listen_port=PROXY_PORT)
+            opts.add_option('body_size_limit', int, 0, '')
+            opts.add_option('keep_host_header', bool, True, '')
+            pconf = proxy.config.ProxyConfig(opts)
+
+            dump_master = DumpMaster(None)
+            dump_master.server = proxy.server.ProxyServer(pconf)
+            dump_master.addons.add(_InjectScripts())
+            dump_master.run()
+
+        process = Process(target=_wrapper)
+        process.start()
+        self._proxy_process = process
+
+
+    def _get_web_driver(self, headless):
+        user_agent = '''
+            Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/60.0.3112.50 Safari/537.36
+        '''
+
+        chrome_options = webdriver.ChromeOptions()
+        if headless:
+            chrome_options.add_argument('headless')
+        proxy_str = '--proxy-server=http://{0}:{1}'.format(PROXY_HOST, str(PROXY_PORT))
+        chrome_options.add_argument(proxy_str)
+        chrome_options.add_argument('user-agent={0}'.format(user_agent))
+        driver = webdriver.Chrome(CHROME_DRIVER, chrome_options=chrome_options)
+
+        self._driver = driver
+
+
+class _InjectScripts:
+
+    def __init__(self):
+        current_directory = os.path.dirname(__file__)
+        js_file = os.path.join(current_directory, JS_FILE)
+        js_to_execute = open(js_file).read()
+        self._js = js_to_execute
+
+
+    def response(self, flow: http.HTTPFlow):
+        ct_header = 'Content-Type'
+        if (ct_header in flow.response.headers and \
+                flow.response.headers[ct_header] != 'text/html') or \
+                not flow.response.status_code == 200:
+            return
+
+        html = BeautifulSoup(flow.response.text, 'lxml')
+        container = html.head or html.body
+        if container:
+            script = html.new_tag('script', type='text/javascript')
+            script.string = self._js
+            container.insert(0, script)
+            flow.response.text = str(html)
