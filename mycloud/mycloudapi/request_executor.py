45b033dd2674d8e55e8017ab8488a306ff982120
Thomas Gassmann <thomas.gassmann@hotmail.com>
2019-03-29 19:48:34 +0100

    fix user agent

--- mycloud/mycloudapi/request_executor.py
+++ mycloud/mycloudapi/request_executor.py
@@ -72,9 +72,7 @@ class MyCloudRequestExecutor:
         headers = requests.utils.default_headers()
         headers['Content-Type'] = content_type
         headers['Authorization'] = 'Bearer ' + bearer_token
-        headers['User-Agent'] = '''
-            Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36
-        '''
+        headers['User-Agent'] = 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/56.0.2924.87 Safari/537.36'
         return headers
 
 
