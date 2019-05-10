c2217c24b0110a1b5690eae42f1652ad876f9878
Thomas Gassmann <thomas.gassmann@hotmail.com>
2019-05-11 00:05:34 +0200

    migrate changes command

--- mycloud/commands/statistics.py
+++ mycloud/commands/statistics.py
@@ -1,5 +1,6 @@
 import click
 from mycloud.statistics.summarizer import summarize
+from mycloud.statistics.change_tracker import track_changes
 from mycloud.mycloudapi import MyCloudRequestExecutor
 
 
@@ -10,7 +11,16 @@ def statistics_command():
 
 @statistics_command.command()
 @click.pass_context
-@click.argument('dir', required=True)
+@click.argument('dir', str, required=True)
 def summary(ctx, dir: str):
     request_executor = ctx.obj['injector'].provide(MyCloudRequestExecutor)
     summarize(request_executor, dir)
+
+
+@statistics_command.command()
+@click.pass_context
+@click.argument('dir', str, required=True)
+@click.argument('top', int, required=False, default=10)
+def changes(ctx, dir: str, top: int):
+    request_executor = ctx.obj['injector'].provide(MyCloudRequestExecutor)
+    track_changes(request_executor, dir, top)
--- mycloud/statistics/statistics_command_line_parser.py
+++ mycloud/statistics/statistics_command_line_parser.py
@@ -1,7 +1,5 @@
 import argparse
 from mycloud.logger import log
-from mycloud.statistics.summarizer import summarize
-from mycloud.statistics.change_tracker import track_changes
 from mycloud.statistics.usage import print_usage
 from mycloud.statistics.size import calculate_size
 
@@ -28,20 +26,6 @@ class StatisticsCommandLineParser:
             parser.print_help()
             exit(1)
         getattr(self, parsed.command)(args[1:])
-
-    def changes(self, args):
-        parser = argparse.ArgumentParser(
-            description='Swisscom myCloud Change Detection', formatter_class=argparse.RawTextHelpFormatter)
-        self.app._add_remote_directory_argument(parser)
-        self.app._add_token_argument(parser)
-        self.app._add_log_file_argument(parser)
-        self.app._add_user_name_password(parser)
-        parser.add_argument(
-            '--top', help='Amount of changes to list', type=int, required=True)
-        args = parser.parse_args(args)
-        executor = self.app._get_request_executor(args)
-        self.app._set_log_file(args.log_file)
-        track_changes(executor, args.mycloud_dir, args.top)
 
     def usage(self, args):
         parser = argparse.ArgumentParser(
