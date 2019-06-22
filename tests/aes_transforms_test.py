1b3a8959f42dfb3a708115f65fdf066b2fc75ae7
Thomas Gassmann <thomas.gassmann@hotmail.com>
2019-06-23 01:08:17 +0200

    add basic testing infrastructure with pytest

---
+++ tests/aes_transforms_test.py
@@ -0,0 +1,16 @@
+import os
+from mycloud.streamapi.transforms import AES256CryptoTransform
+
+
+def test_aes_transform_padding_is_applied():
+    transform = AES256CryptoTransform('test')
+    first_bytes = os.urandom(1024)
+    second_bytes = os.urandom(1001)
+    first = transform.up_transform(first_bytes)
+    second_with_padding = transform.up_transform(second_bytes, last=True)
+
+    transform.reset_state()
+
+    first_down = transform.down_transform(first)
+    second_down = transform.down_transform(second_with_padding, last=True)
+    assert first_down + second_down == first_bytes + second_bytes
