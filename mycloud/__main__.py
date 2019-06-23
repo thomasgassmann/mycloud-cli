f0e422bdccb2cad5d21ee33fbc45fe8ed04e19be
Thomas Gassmann <thomas.gassmann@hotmail.com>
2019-06-23 23:35:32 +0200

    move drive-related commands into separate sub-module

--- mycloud/__main__.py
+++ mycloud/__main__.py
@@ -2,7 +2,9 @@ import logging
 import click
 from mycloud.commands import (
     auth_command,
-    statistics_command,
+    statistics_command
+)
+from mycloud.commands.drive import (
     upload_command,
     download_command,
     convert_command,
@@ -18,18 +20,27 @@ from mycloud.pinject import build_container
 def mycloud_cli(ctx, token: str, log_level: str):
     try:
         level = getattr(logging, log_level) if log_level else logging.INFO
-        logging.basicConfig(level=level)
+        logging.basicConfig(
+            level=level,
+            format="%(asctime)s [%(threadName)-12.12s] [%(levelname)-5.5s]  %(message)s")
     except AttributeError:
         raise click.ClickException(f'Log level {log_level} not found.')
     ctx.obj['injector'] = build_container(token=token)
 
 
+@click.group(name='drive')
+def drive_cli():
+    pass
+
+
+drive_cli.add_command(delete_command)
+drive_cli.add_command(upload_command)
+drive_cli.add_command(download_command)
+drive_cli.add_command(convert_command)
+
+mycloud_cli.add_command(drive_cli)
 mycloud_cli.add_command(auth_command)
 mycloud_cli.add_command(statistics_command)
-mycloud_cli.add_command(upload_command)
-mycloud_cli.add_command(download_command)
-mycloud_cli.add_command(convert_command)
-mycloud_cli.add_command(delete_command)
 
 if __name__ == '__main__':
     mycloud_cli(obj={})
--- mycloud/commands/__init__.py
+++ mycloud/commands/__init__.py
@@ -1,6 +1,2 @@
 from mycloud.commands.auth import auth_command
 from mycloud.commands.statistics import statistics_command
-from mycloud.commands.upload import upload_command
-from mycloud.commands.download import download_command
-from mycloud.commands.convert import convert_command
-from mycloud.commands.delete import delete_command
